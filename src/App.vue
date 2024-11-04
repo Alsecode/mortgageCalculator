f2fd9afcf9df6e7a7f6b89aa500be59d21752e4b
Alsecode <adementor@yandex.ru>
2024-11-04 16:08:25 +0300

    Добавление начальных файлов проекта

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="main-container">
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  font-size: 14px;
+	font-family: "Montserrat", sans-serif;
+}
+
+:root {
+  --picton-blue: #24ADED;
+  --denim: #108EC9;
+  --white: #FFFFFF;
+  --zircon: #F3F6FF;
+  --fog: #DAE3FF;
+  --athens-gray: #F0F1F4;
+  --oslo-gray: #93989D;
+  --regent-gray: #8593A5;
+  --big-stone: #15203D;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url("./assets/fonts/Montserrat/Montserrat-Regular.ttf"), Verdana, sans-serif;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url("./assets/fonts/Montserrat/Montserrat-Medium.ttf"), Verdana, sans-serif;
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url("./assets/fonts/Montserrat/Montserrat-Bold.ttf"), Verdana, sans-serif;
+  font-weight: 700;
+}
+
+.main-container {
+  width: 100%;
+  max-width: 1000px;
+  margin: 20px auto;
+}
+</style>
